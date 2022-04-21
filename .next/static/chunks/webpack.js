22e60d5b2e988866bb2b6a43766a1fcdd9a4834f
diegodevgt <88571283+diegodevgt@users.noreply.github.com>
2022-04-21 01:45:27 -0600

    components

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0753eb87d261340d"; }
+/******/ 		__webpack_require__.h = function() { return "142652511bced2ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Descripcion from '../components/Descripcion'
+// import Descripcion from '../components/Descripcion'
 
 export default function Home() {
   return (
@@ -20,7 +20,7 @@ export default function Home() {
         
       </main>
 
-      <Descripcion />
+      {/* <Descripcion /> */}
 
     </div>
   )
